c1a044fb9ad15ecef66b575f8554c75ec2eaeb23
Yurii Habor <yurahabor992@gmail.com>
2023-07-13 20:53:13 +0300

    adding_nav_graph_logic

--- app/src/main/java/com/example/pizzaapp/SummaryFragment.kt
+++ app/src/main/java/com/example/pizzaapp/SummaryFragment.kt
@@ -19,5 +19,5 @@ class SummaryFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
-    
+
 }
